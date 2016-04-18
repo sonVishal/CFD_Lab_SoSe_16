9179d784f269ec5d8a6567515acaa38be35acd4f
Daniel Lehmberg <d.lehmberg@tum.de>
2016-04-18 23:09:35 +0200

    initial implementation 'calculate_dt'

--- src/uvp.c
+++ src/uvp.c
@@ -1,4 +1,7 @@
 #include "uvp.h"
+#include <math.h>
+#include <float.h>
+
 
 void calculate_fg(
   double Re,
@@ -31,6 +34,27 @@ void calculate_rs(
 	/* TODO: implement */
 }
 
+
+
+double p_find_max(double **matrix, int imax, int jmax){
+	int i,j;
+
+	/* equals: minimum (negative) double value; note the "-" (!!)
+	 * https://stackoverflow.com/questions/1153548/minimum-double-value-in-c-c*/
+	double found_max = -DBL_MAX;
+
+
+	/*TODO: (DL) this is most likely wrong, check out again how the matrix is structured */
+	for(i = 0; i<imax; ++i){
+		for(j = 0; j < jmax; ++j){
+			if(found_max < matrix[i][j]){
+				found_max = matrix[i][j];
+			}
+		}
+	}
+	return found_max;
+}
+
 void calculate_dt(
   double Re,
   double tau,
@@ -42,6 +66,37 @@ void calculate_dt(
   double **U,
   double **V
 ){
-	/* TODO: implement */
+	double restriction; /* holds the currently tested restriction */
+	double min;
+	restriction = Re/(2 * (1/(dx*dx) + 1/(dy*dy)));
+	min = restriction;
+
+	restriction = dx / p_find_max(U, imax, jmax);
+	if(min > restriction){
+		min = restriction;
+	}
+
+	restriction = dy / p_find_max(V, imax, jmax);
+	if(min > restriction){
+		min = restriction;
+	}
+
+	*dt = tau*min;
 }
+
+void calculate_uv(
+  double dt,
+  double dx,
+  double dy,
+  int imax,
+  int jmax,
+  double **U,
+  double **V,
+  double **F,
+  double **G,
+  double **P
+){
+	/* TODO implement */
+}
+
 
