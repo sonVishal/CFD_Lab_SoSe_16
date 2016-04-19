c5c17310a1d6d04f717be1c59f9a66877363f9b9
sonVishal <sontakke.vishal1991@gmail.com>
2016-04-19 14:00:00 +0200

    Implemented the boundaryvalues() function

--- src/boundary_val.c
+++ src/boundary_val.c
@@ -6,7 +6,23 @@ void boundaryvalues(
   double **U,
   double **V
 ){
-	/*TODO implement*/
+    for (int i = 1; i <= imax; i++) {
+        // Eq(15)
+        V[i][0]         =   0.0;
+        V[i][jmax]      =   0.0;
+
+        // Eq(16)
+        U[i][0]         =   -U[i][1];
+        U[i][jmax+1]    =   -U[i][jmax];
+    }
+
+    for (int j = 1; j <= jmax; j++) {
+        // Eq(15)
+        U[0][j]         =   0.0;
+        U[imax][j]      =   0.0;
+
+        // Eq(16)
+        V[0][j]         =   -V[1][j];
+        V[imax+1][j]    =   -V[imax][j];
+    }
 }
-
-
