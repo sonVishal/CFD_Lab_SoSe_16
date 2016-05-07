df0b9e8129ca94dfac7c57f711eed8866fa31724
tordks <tordks@hotmail.com>
2016-05-07 18:44:57 +0200

    Add readParameters

--- srcLB/initLB.c
+++ srcLB/initLB.c
@@ -1,9 +1,22 @@
 #include "initLB.h"
 
 int readParameters(int *xlength, double *tau, double *velocityWall, int *timesteps, int *timestepsPerPlotting, int argc, char *argv[]){
-  /* TODO */
 
+    double xvelocity, yvelocity, zvelocity;
 
+    READ_INT(*argv, *xlength);
+    READ_DOUBLE(*argv, *tau);
+
+    READ_DOUBLE(*argv, xvelocity);
+    READ_DOUBLE(*argv, yvelocity);
+    READ_DOUBLE(*argv, zvelocity);
+
+    velocityWall[0] = xvelocity;
+    velocityWall[1] = yvelocity;
+    velocityWall[2] = zvelocity;
+
+    READ_INT(*argv, *timesteps);
+    READ_INT(*argv, *timestepsPerPlotting);
 
   return 0;
 }
--- srcLB/main.c
+++ srcLB/main.c
@@ -14,32 +14,33 @@ int main(int argc, char *argv[]){
 
     /* TODO: (DL) in the worksheet there is "flagField" and "flagfield" -
      * for the moment I think they are the same... */
-    int *flagField=NULL;
+    //int *flagField=NULL;
     int xlength;
     double tau;
     double velocityWall[3];
     int timesteps;
     int timestepsPerPlotting;
 
-    readParameters(&xlength, &tau, &velocityWall, &timesteps, &timestepsPerPlotting,argc, argv);
+    readParameters(&xlength, &tau, velocityWall, &timesteps, &timestepsPerPlotting,argc, &argv[1]);
+
     // TODO: initialise pointers here!
 
-    initialiseFields(collideField, streamField, flagField, xlength);
+    //initialiseFields(collideField, streamField, flagField, xlength);
 
     for(int t = 0; t < timesteps; t++){
-	double *swap=NULL;
-	doStreaming(collideField,streamField,flagField,xlength);
+	    double *swap=NULL;
+	    //doStreaming(collideField,streamField,flagField,xlength);
 
-	swap = collideField;
-	collideField = streamField;
-	streamField = swap;
+	    swap = collideField;
+	    collideField = streamField;
+	    streamField = swap;
 
-	doCollision(collideField,flagField,&tau,xlength);
-	treatBoundary(collideField,flagField,velocityWall,xlength);
+	    //doCollision(collideField,flagField,&tau,xlength);
+	    //treatBoundary(collideField,flagField,velocityWall,xlength);
 
-	if (t%timestepsPerPlotting==0){
-	    writeVtkOutput(collideField,flagField,argv,t,xlength);
-	}
+	    if (t%timestepsPerPlotting==0){
+	        //writeVtkOutput(collideField,flagField,argv,t,xlength);
+	    }
     }
     return 0;
 }
