a36027cabc490be04ac74f17bec0b9ace1479329
Daniel Lehmberg <d.lehmberg@tum.de>
2016-05-07 17:40:15 +0200

    main as provided in the worksheet

--- srcLB/main.c
+++ srcLB/main.c
@@ -8,10 +8,36 @@
 #include "visualLB.h"
 
 int main(int argc, char *argv[]){
-  /* TODO */
 
+    double *collideField=NULL;
+    double *streamField=NULL;
 
-  return 0;
+    /* TODO: (DL) in the worksheet there is "flagField" and "flagfield" -
+     * for the moment I think they are the same... */
+    int *flagfield=NULL;
+    int xlength; double tau;
+    double velocityWall[3];
+    int timesteps;
+    int timestepsPerPlotting;
+
+    readParameters( &xlength,&tau,&velocityWall,timesteps,timestepsPerPlotting,argc, argv);
+    // TODO: initialise pointers here!
+
+    initialiseFields(collideField,streamField,flagfield,xlength);
+
+    for(int t = 0; t < timesteps; t++){
+	double *swap=NULL;
+	doStreaming(collideField,streamField,flagfield,xlength);
+	swap = collideField; collideField = streamField;
+	streamField = swap;
+	doCollision(collideField,flagfield,&tau,xlength);
+	treatBoundary(collideField,flagfield,velocityWall,xlength);
+
+	if (t%timestepsPerPlotting==0){
+	    writeVtkOutput(collideField,flagfield,argv,t,xlength);
+	}
+    }
+    return 0;
 }
 
 #endif
