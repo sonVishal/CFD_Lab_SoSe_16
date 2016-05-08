fc3375a23678da928525fe0c572c4d85c8f7d849
tordks <tordks@hotmail.com>
2016-05-08 20:03:53 +0200

    Variable name change

--- srcLB/streaming.c
+++ srcLB/streaming.c
@@ -11,19 +11,8 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
 	 * First: profile and see how this function adds to total workload (check if it's worth it)
 	 */
 
-    //TODO: (TKS) Found the whole offset notation a bit confusing.
-    //            make a variable called index in the inner loop. That feels
-    //            more descriptive. index gives the index of the first 
-    //            direction in a cell at position (x,y,z)
-    //
-    //            Suggested name changes:
-    //            cell_xyzoffset --> index or current_cell_index
-    //            n_xyzoffset --> n_index or n_cell_index
-    //
-    //
-
-	int zoffset, yzoffset, xyzoffset, cell_xyzoffset; //current cell offset values
-	int n_x, n_y, n_z, n_xyzoffset; 				  //neighbor offset values
+	int zoffset, yzoffset, xyzoffset, current_cell_index; //current cell offset values
+	int n_x, n_y, n_z, n_cell_index; 				  //neighbor offset values
 
 	for(int z=1; z<=xlength; z++){
 		zoffset = z*xlength;
@@ -40,16 +29,16 @@ void doStreaming(double *collideField, double *streamField,int *flagField,int xl
 				// printf("Condition check %i \n", flagField[xyzoffset]);
 
 				if( ! flagField[xyzoffset] ){ //true if FLUID cell
-					cell_xyzoffset = Q*xyzoffset;
+					current_cell_index = Q*xyzoffset;
 
 					//loop through all neighbors and copy the respective distribution
 					for(int i=0; i<Q; ++i){
 						n_x = x+LATTICEVELOCITIES[i][0];
 						n_y = y+LATTICEVELOCITIES[i][1];
 						n_z = z+LATTICEVELOCITIES[i][2];
-						n_xyzoffset = Q*(xlength*(n_z*xlength + n_y) + n_x);
+						n_cell_index = Q*(xlength*(n_z*xlength + n_y) + n_x);
 
-						streamField[cell_xyzoffset + i] = collideField[n_xyzoffset + (Q-i-1)];
+						streamField[current_cell_index + i] = collideField[n_cell_index + (Q-i-1)];
 					}
 				}
 			}
