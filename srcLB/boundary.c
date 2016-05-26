2cb99a6d88220a03ca82eba385127c1f82c21d73
Daniel Lehmberg <d.lehmberg@tum.de>
2016-05-26 16:30:21 +0200

    add todo

--- srcLB/boundary.c
+++ srcLB/boundary.c
@@ -260,6 +260,7 @@ void p_freeSlip(double* collideField, t_flagField const * const flagField,
 
 	p_assignIndices(&flagField[currentFlagIndex].position,index,mirrorIndex);
 
+	//TODO:(DL) leave valid check? it's by construction never out of bound
     if(nextCellIndex >= 0 && nextCellIndex < *totalSize &&
             flagField[nextFlagIndex].type == FLUID) {
         for (i = 0; i < 5; i++) {
