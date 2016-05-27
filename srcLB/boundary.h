1080584a22e2ab49638661447f99246663a9433c
sonVishal <vishal.sontakke@tum.de>
2016-05-27 15:35:09 +0200

    Remove private functions from h file

--- srcLB/boundary.h
+++ srcLB/boundary.h
@@ -6,29 +6,4 @@
 void treatBoundary(double *collideField, const t_flagField * const flagField,
 	const t_boundPara * const boundPara, const int * const xlength);
 
-void p_noSlip(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	int const * const totalSize);
-
-void p_movingWall(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	const t_boundPara * const boundPara, int const * const totalSize);
-
-void p_freeSlip(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	int const * const totalSize);
-
-void p_outflow(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	const t_boundPara * const boundPara, int const * const totalSize);
-
-void p_inflow(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	const t_boundPara * const boundPara);
-
-void p_pressureIn(double* collideField, t_flagField const * const flagField,
-	int const * const point, int const * const xlength,
-	const t_boundPara * const boundPara, int const * const totalSize);
-
-void p_assignIndices(const short int * const flag, int * index, int * mirrorIndex);
 #endif
