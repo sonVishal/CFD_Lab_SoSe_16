2e168bc802bffa6b168579b0fd556d55138f43e9
Daniel Lehmberg <d.lehmberg@tum.de>
2016-05-29 11:37:43 +0200

    remove todo and fixed matlab file

--- worksheet3/CreateScenario/Bottleneck.m
+++ worksheet3/CreateScenario/Bottleneck.m
@@ -1,28 +1,21 @@
-%depth is y-axes, that means view is on x-z plane (looking from the side)
-% the boundaries on top and 
-%y-z plane where x = {0,xmax} is not given and set in the simulation
 clear all;
 close all;
 
 x_len = 210;
 z_len = 490;
 
-scen = zeros(x_len, z_len); %+2 is ghoast layers
+scen = zeros(x_len, z_len);
 
 bottom_w = 200;
 top_w = 30;
 
 start = 100;
 
-
 for i = 1:(bottom_w-top_w)/2
-    
     for j = 1:bottom_w-2*i
         scen(i, start+i+j) = 1;
-        scen(x_len-i, start+i+j) = 1;
-        
+        scen(x_len-i+1, start+i+j) = 1;
     end
-    
 end
 
 
