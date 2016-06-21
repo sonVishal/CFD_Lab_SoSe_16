83352cf0670ae4737e76f35b8f27f06cbbf44976
tordks <tordks@hotmail.com>
2016-06-21 20:50:46 +0200

    Started tidying boundary indent

--- srcProject/boundary.c
+++ srcProject/boundary.c
@@ -11,219 +11,216 @@ However, leave it as long as possible for testing and compare with previous scen
 void p_setBounceBack(double *collideField, const double * const wallVelocity,
 	const int type, const int i, const int current_cell_index, const int n_cell_index, const int *c) {
 
-		if(type == 1){ 	 //bounce back
-			/* equation 17 */
-			collideField[current_cell_index + i] = collideField[n_cell_index + (Q-i-1)];
-		}else{ // type == 2 (MOVING_WALL)
+    if(type == 1){ 	 //bounce back
+        /* equation 17 */
+        collideField[current_cell_index + i] = collideField[n_cell_index + (Q-i-1)];
+    }else{ // type == 2 (MOVING_WALL)
 
-			double density = 0;
-			computeDensity(&collideField[n_cell_index], &density);
+        double density = 0;
+        computeDensity(&collideField[n_cell_index], &density);
 
-			double dot_uwall_c = wallVelocity[0]*c[0]+wallVelocity[1]*c[1]+wallVelocity[2]*c[2];
-			double weight = LATTICEWEIGHTS[i];
+        double dot_uwall_c = wallVelocity[0]*c[0]+wallVelocity[1]*c[1]+wallVelocity[2]*c[2];
+        double weight = LATTICEWEIGHTS[i];
 
-			/* equation 19 */
-			collideField[current_cell_index + i] = collideField[n_cell_index + (Q-i-1)] +
-			2 * weight * density * dot_uwall_c / (C_S*C_S);
-		}
-	}
+        /* equation 19 */
+        collideField[current_cell_index + i] = collideField[n_cell_index + (Q-i-1)] +
+        2 * weight * density * dot_uwall_c / (C_S*C_S);
+    }
+}
 
-	/* Helper function similar to p_computeCellOffset, except that it computes the offset value
-	* of a neighboring cell in the direction of 'c_vec'.
-	* There are no checks that guarantee that the computed offset value is valid.
-	* For example, when the current cell is on the boundary and 'c_vec' points away from the inner value.
-	*/
-	int p_computeNeighborCellOffset(int outer, int inner, int fixedValue,
-		const int * const c_vec, int const * const xlength, const int wallIdx){
+/* Helper function similar to p_computeCellOffset, except that it computes the offset value
+* of a neighboring cell in the direction of 'c_vec'.
+* There are no checks that guarantee that the computed offset value is valid.
+* For example, when the current cell is on the boundary and 'c_vec' points away from the inner value.
+*/
+int p_computeNeighborCellOffset(int outer, int inner, int fixedValue,
+    const int * const c_vec, int const * const xlength, const int wallIdx){
 
-			switch (wallIdx/2) {
-				case 0: // LEFT, RIGHT -> Y fixed
-				outer 		+= c_vec[2];	//= Z
-				inner 		+= c_vec[0];	//= X
-				fixedValue 	+= c_vec[1];	//= Y
-				return (xlength[0]+2) * (outer * (xlength[1]+2) + fixedValue) + inner;
+    switch (wallIdx/2) {
+        case 0: // LEFT, RIGHT -> Y fixed
+            outer 		+= c_vec[2];	//= Z
+            inner 		+= c_vec[0];	//= X
+            fixedValue 	+= c_vec[1];	//= Y
+            return (xlength[0]+2) * (outer * (xlength[1]+2) + fixedValue) + inner;
 
-				case 1: // TOP, BOTTOM -> Z fixed
-				outer 		+= c_vec[1];	//= Y
-				inner 		+= c_vec[0];	//= X
-				fixedValue 	+= c_vec[2];	//= Z
-				return (xlength[0]+2) * (fixedValue * (xlength[1]+2) + outer) + inner;
+        case 1: // TOP, BOTTOM -> Z fixed
+            outer 		+= c_vec[1];	//= Y
+            inner 		+= c_vec[0];	//= X
+            fixedValue 	+= c_vec[2];	//= Z
+            return (xlength[0]+2) * (fixedValue * (xlength[1]+2) + outer) + inner;
 
-				case 2: // FRONT, BACK -> X fixed
-				outer 		+= c_vec[2];	//= Z
-				inner 		+= c_vec[1];	//= Y
-				fixedValue 	+= c_vec[0];	//= X
-				return (xlength[0]+2) * (outer * (xlength[1]+2) + inner) + fixedValue;
+        case 2: // FRONT, BACK -> X fixed
+            outer 		+= c_vec[2];	//= Z
+            inner 		+= c_vec[1];	//= Y
+            fixedValue 	+= c_vec[0];	//= X
+            return (xlength[0]+2) * (outer * (xlength[1]+2) + inner) + fixedValue;
 
-				default:
-				ERROR("Invalid wall index occured. This should not happen !!!");
-				return -1;
-			}
-		}
-
-
-		/* Helper function that treats a single boundary wall. The wall itself is described with wallIdx
-		* (from enum WALLS) and the 'fixedValue' (generally 0 or xlength+1).
-		*/
-
-		void p_treatSingleWall(double *collideField, const int * const flagField, const t_procData * const procData, const int wallIdx){
-
-			int endOuter = -1, endInner = -1, fixedValue = -1;
-			p_setIterationParameters(&endOuter, &endInner, &fixedValue, procData, wallIdx);
-			assert(endOuter != -1 && endInner != -1 && fixedValue != -1);
-
-			int xlength[3] = {procData->xLength[0], procData->xLength[1], procData->xLength[2]};
-
-			//variable needed at check whether it is an in-domain (FLUID) cell
-			int maxValidIndex = Q*(xlength[0]+2)*(xlength[1]+2)*(xlength[2]+2);
-
-			//k - corresponds to the 'outer' value when computing the offset
-			for(int k = 0; k <= endOuter; ++k){
-				//j - corresponds to the 'inner' value
-				for(int j = 0; j <= endInner; ++j){
-
-					//flagField[xyz_offset] should only be a boundary cell (checked in p_setBounceBack)
-					int xyz_offset = p_computeCellOffset(k, j, fixedValue, xlength, wallIdx);
-					int current_cell_index = Q*xyz_offset;
-					int boundaryType = flagField[xyz_offset];
-
-					assert(boundaryType == NO_SLIP || boundaryType == MOVING_WALL);
-
-					//PARALLEL boundaries are not treated when they occur (e.g. at shared edges)
-					for(int i = 0; i < Q; ++i){
-						int c[3] = {LATTICEVELOCITIES[i][0], LATTICEVELOCITIES[i][1], LATTICEVELOCITIES[i][2]};
-						int n_xyzoffset = p_computeNeighborCellOffset(k, j, fixedValue, c, xlength, wallIdx);
-						int n_cell_index = Q*n_xyzoffset;
-
-						//check (1) valid index: in case the direction of vector 'c' points to a non-existing cell
-						//check (2) that the neighboring cell is a FLUID cell or PARALLEL_BOUNDARY (treated as inner)
-						if(n_cell_index >= 0 && n_cell_index < maxValidIndex &&
-							(flagField[n_xyzoffset] == FLUID || flagField[n_xyzoffset] == PARALLEL_BOUNDARY)
-						){
-							p_setBounceBack(collideField, procData->wallVelocity, boundaryType, i, current_cell_index, n_cell_index, c);
-						}
-					}
-				}
-			}
-		}
+        default:
+            ERROR("Invalid wall index occured. This should not happen !!!");
+            return -1;
+    }
+}
 
 
-		void extractEdge(double sendBuffer[], double const * const collideField, t_iterParaEdge const * const iterPara, t_procData const * const procData,
+/* Helper function that treats a single boundary wall. The wall itself is described with wallIdx
+* (from enum WALLS) and the 'fixedValue' (generally 0 or xlength+1).
+*/
+
+void p_treatSingleWall(double *collideField, const int * const flagField, const t_procData * const procData, const int wallIdx){
+
+    int endOuter = -1, endInner = -1, fixedValue = -1;
+    p_setIterationParameters(&endOuter, &endInner, &fixedValue, procData, wallIdx);
+    assert(endOuter != -1 && endInner != -1 && fixedValue != -1);
+
+    int xlength[3] = {procData->xLength[0], procData->xLength[1], procData->xLength[2]};
+
+    //variable needed at check whether it is an in-domain (FLUID) cell
+    int maxValidIndex = Q*(xlength[0]+2)*(xlength[1]+2)*(xlength[2]+2);
+
+    //k - corresponds to the 'outer' value when computing the offset
+    for(int k = 0; k <= endOuter; ++k){
+        //j - corresponds to the 'inner' value
+        for(int j = 0; j <= endInner; ++j){
+
+            //flagField[xyz_offset] should only be a boundary cell (checked in p_setBounceBack)
+            int xyz_offset = p_computeCellOffset(k, j, fixedValue, xlength, wallIdx);
+            int current_cell_index = Q*xyz_offset;
+            int boundaryType = flagField[xyz_offset];
+
+            assert(boundaryType == NO_SLIP || boundaryType == MOVING_WALL);
+
+            //PARALLEL boundaries are not treated when they occur (e.g. at shared edges)
+            for(int i = 0; i < Q; ++i){
+                int c[3] = {LATTICEVELOCITIES[i][0], LATTICEVELOCITIES[i][1], LATTICEVELOCITIES[i][2]};
+                int n_xyzoffset = p_computeNeighborCellOffset(k, j, fixedValue, c, xlength, wallIdx);
+                int n_cell_index = Q*n_xyzoffset;
+
+                //check (1) valid index: in case the direction of vector 'c' points to a non-existing cell
+                //check (2) that the neighboring cell is a FLUID cell or PARALLEL_BOUNDARY (treated as inner)
+                if(n_cell_index >= 0 && n_cell_index < maxValidIndex &&
+                    (flagField[n_xyzoffset] == FLUID || flagField[n_xyzoffset] == PARALLEL_BOUNDARY)
+                ){
+                    p_setBounceBack(collideField, procData->wallVelocity, boundaryType, i, current_cell_index, n_cell_index, c);
+                }
+            }
+        }
+    }
+}
+
+
+void extractEdge(double sendBuffer[], double const * const collideField, t_iterParaEdge const * const iterPara, t_procData const * const procData,
 		              const int edge, const int index){
-			int endIndex;
-			const int VARIABLE = -1;
+    int endIndex;
+    const int VARIABLE = -1;
 
-			if(iterPara->x == VARIABLE){
+    if(iterPara->x == VARIABLE){
 
-			}
-
-
-
-		}
+    }
+}
 
 
-		void injectEdge(double readBuffer[], double const * const collideField, t_iterParaEdge const * const iterPara, t_procData const * const procData,
+void injectEdge(double readBuffer[], double const * const collideField, t_iterParaEdge const * const iterPara, t_procData const * const procData,
 						const int edge, const int index){
 			ERROR("IMPLEMENT!!!");
-		}
+}
 
 
-		void p_setBoundaryIterParameters(t_iterPara * const iterPara, t_procData const*const procData, const int direction){
-			switch(direction/2){ //integer division to get the type of face (see enum in LBDefinitions.h)
+void p_setBoundaryIterParameters(t_iterPara * const iterPara, t_procData const*const procData, const int direction){
+    switch(direction/2){ //integer division to get the type of face (see enum in LBDefinitions.h)
 
-				iterPara->startOuter = 1;
-				iterPara->startInner = 1;
+        iterPara->startOuter = 1;
+        iterPara->startInner = 1;
 
-				//---------------------------------------------
-				//outer = Z, inner = X, Y fixed
-				case 0:
-				iterPara->endOuter   = procData->xLength[2];
-				iterPara->endInner   = procData->xLength[0];
-				iterPara->fixedValue = (direction == LEFT) ? 1 : procData->xLength[1];
+        //---------------------------------------------
+        //outer = Z, inner = X, Y fixed
+        case 0:
+        iterPara->endOuter   = procData->xLength[2];
+        iterPara->endInner   = procData->xLength[0];
+        iterPara->fixedValue = (direction == LEFT) ? 1 : procData->xLength[1];
 
-				break;
+        break;
 
-				//---------------------------------------------
-				//outer = Y, inner = X, Z fixed
-				case 1:
-				iterPara->endOuter   = procData->xLength[1];
-				iterPara->endInner   = procData->xLength[0];
-				iterPara->fixedValue = (direction == BOTTOM) ? 1 : procData->xLength[2];
-				break;
+        //---------------------------------------------
+        //outer = Y, inner = X, Z fixed
+        case 1:
+        iterPara->endOuter   = procData->xLength[1];
+        iterPara->endInner   = procData->xLength[0];
+        iterPara->fixedValue = (direction == BOTTOM) ? 1 : procData->xLength[2];
+        break;
 
-				//---------------------------------------------
-				//outer = Z, inner = Y, X fixed
-				case 2:
-				iterPara->endOuter   = procData->xLength[2];
-				iterPara->endInner   = procData->xLength[1];
-				iterPara->fixedValue = (direction == BACK) ? 1 : procData->xLength[0];
-				break;
+        //---------------------------------------------
+        //outer = Z, inner = Y, X fixed
+        case 2:
+        iterPara->endOuter   = procData->xLength[2];
+        iterPara->endInner   = procData->xLength[1];
+        iterPara->fixedValue = (direction == BACK) ? 1 : procData->xLength[0];
+        break;
 
-				default:
-				ERROR("Invalid direction occurred. This should never happen!");
-			}
-		}
+        default:
+        ERROR("Invalid direction occurred. This should never happen!");
+    }
+}
 
-		void p_setEdgeIterParameters(t_iterParaEdge * const iterPara, t_procData const*const procData, const int edge){
-			const int VARIABLE = -1;
+void p_setEdgeIterParameters(t_iterParaEdge * const iterPara, t_procData const*const procData, const int edge){
+    const int VARIABLE = -1;
 
-			switch(edge/4){ //integer division - 12 edges, 3 cases
-				case 0:
-					iterPara->z = 0;
-					if(edge == 0 || edge == 2){
-						iterPara->x = VARIABLE;
-						iterPara->y = (edge == 0) ? 0 : procData->xLength[2];
-					}else{
-						iterPara->y = VARIABLE;
-						iterPara->x = (edge == 1) ? procData->xLength[0] : 1;
-					}
-				break;
+    switch(edge/4){ //integer division - 12 edges, 3 cases
+        case 0:
+            iterPara->z = 0;
+            if(edge == 0 || edge == 2){
+                iterPara->x = VARIABLE;
+                iterPara->y = (edge == 0) ? 0 : procData->xLength[2];
+            }else{
+                iterPara->y = VARIABLE;
+                iterPara->x = (edge == 1) ? procData->xLength[0] : 1;
+            }
+        break;
 
-				case 1:
-					iterPara->z = VARIABLE;
-					iterPara->y = (edge == 4 || edge == 5) ? 1 : procData->xLength[1];
-					iterPara->x = (edge == 4 || edge == 7) ? 1 : procData->xLength[0];
-				break;
+        case 1:
+            iterPara->z = VARIABLE;
+            iterPara->y = (edge == 4 || edge == 5) ? 1 : procData->xLength[1];
+            iterPara->x = (edge == 4 || edge == 7) ? 1 : procData->xLength[0];
+        break;
 
-				case 2:
-					iterPara->z = procData->xLength[2];
-					if(edge == 0 || edge == 2){
-						iterPara->x = VARIABLE;
-						iterPara->y = edge == 0 ? 0 : procData->xLength[2];
-					}else{
-						iterPara->y = VARIABLE;
-						iterPara->x = edge == 1 ? procData->xLength[0] : 0;
-					}
-				break;
+        case 2:
+            iterPara->z = procData->xLength[2];
+            if(edge == 0 || edge == 2){
+                iterPara->x = VARIABLE;
+                iterPara->y = edge == 0 ? 0 : procData->xLength[2];
+            }else{
+                iterPara->y = VARIABLE;
+                iterPara->x = edge == 1 ? procData->xLength[0] : 0;
+            }
+        break;
 
-				default:
-					ERROR("TODO: msg");
-			}
-	}
+        default:
+            ERROR("TODO: msg");
+    }
+}
 
-	void p_assignSharedEdgeIndex(const int edge, int *index) {
+void p_assignSharedEdgeIndex(const int edge, int *index) {
 
-		//Look for edges numbering in LBDefinitions.h
-		// edge 0: (0,-1,-1) 	-> [0]
-		// edge 1: (1,0,-1) 	-> [3]
-		// edge 2: (0,1,-1) 	-> [4]
-		// edge 3: (-1,0,-1) 	-> [1]
-		//
-		// edge 4: (-1,-1,0) 	-> [5]
-		// edge 5: (1,-1,0) 	-> [7]
-		// edge 6: (1,1,0) 		-> [13]
-		// edge 7: (-1,1,0) 	-> [11]
-		//
-		// edge 8: (0,-1,1) 	-> [14]
-		// edge 9: (1,0,1) 		-> [17]
-		// edge 10: (0,1,1) 	-> [18]
-		// edge 11: (-1,0,1) 	-> [15]
-		static int indices[12] = {0,3,4,1,
-			5,7,13,11,
-			14,17,18,15};
+    //Look for edges numbering in LBDefinitions.h
+    // edge 0: (0,-1,-1) 	-> [0]
+    // edge 1: (1,0,-1) 	-> [3]
+    // edge 2: (0,1,-1) 	-> [4]
+    // edge 3: (-1,0,-1) 	-> [1]
+    //
+    // edge 4: (-1,-1,0) 	-> [5]
+    // edge 5: (1,-1,0) 	-> [7]
+    // edge 6: (1,1,0) 		-> [13]
+    // edge 7: (-1,1,0) 	-> [11]
+    //
+    // edge 8: (0,-1,1) 	-> [14]
+    // edge 9: (1,0,1) 		-> [17]
+    // edge 10: (0,1,1) 	-> [18]
+    // edge 11: (-1,0,1) 	-> [15]
+    static int indices[12] = {0,  3,  4,  1,
+                              5,  7,  13, 11,
+                              14, 17, 18, 15};
 
-			index[0] = indices[edge];
-		}
+        index[0] = indices[edge];
+}
 
 		void treatBoundary(t_component *c, int const * const flagField, double *collideField, const t_procData * const procData, double **sendBuffer, double **readBuffer){
 			// const int NO_NEIGHBOUR = -2; // equals the MPI_PROC_NULL = -2
