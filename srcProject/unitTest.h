6c3bf71230ad41f404591bdf1148f3ee2a2cb3fe
sonVishal <vishal.sontakke@tum.de>
2016-06-23 16:01:00 +0200

    Add unit-test

---
+++ srcProject/unitTest.h
@@ -0,0 +1,22 @@
+#ifndef _UNITTEST_H_
+#define _UNITTEST_H_
+#include "LBDefinitions.h"
+#include "computeCellValues.h"
+#include <mpi/mpi.h>
+
+#define _TOL_ 1e-15
+
+void storeMassVector(const t_component * const c, const int numComp, double ** massVector,
+    const int * const xlength);
+
+void checkMassVector(double *massVectorBefore[], double *massVectorAfter[], const int * const xlength,
+    const int numComp, const int rank);
+
+void computeCellMomentum(const double * const currentCell, double *momentum);
+
+void computeGlobalMomentum(const t_component * const c, const int numComp,
+    const int * const xlength, double * compMomentum);
+
+void checkMomentum(const double * const momentumBefore, const double * const momentumAfter,
+    const int numComp);
+#endif /* end of include guard: _UNITTEST_H_ */
