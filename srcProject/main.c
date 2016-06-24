5d0336c358763f7d9be9088874be6a5e9592dbdb
tordks <tordks@hotmail.com>
2016-06-24 20:12:27 +0200

    edit TODO

--- srcProject/main.c
+++ srcProject/main.c
@@ -14,6 +14,7 @@
 
 
 //TODO: (TKS) Where should these swap functions be placed?
+//          * Make a common function file
 void swapComponentFields(t_component *c, int numComp){
     for (int i = 0; i < numComp; ++i) {
         double* swap = c[i].collideField;
