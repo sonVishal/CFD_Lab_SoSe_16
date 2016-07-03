ca876ee9b417ad148face9f8182be09c79311ade
tordks <tordks@hotmail.com>
2016-07-03 18:34:35 +0200

    Added header

--- srcLB/main.c
+++ srcLB/main.c
@@ -3,6 +3,7 @@
 
 #include "boundary.h"
 #include "collision.h"
+#include "streamCollide.h"
 #include "initLB.h"
 #include "streaming.h"
 #include "visualLB.h"
@@ -14,38 +15,6 @@
 //TODO: (TKS) temporarily have the streamingcollide func here.
 //      * Should rename stream and collidefield
 
-void streamCollide(int xlength, double* streamField, double* collideField, double* feq, int* flagField){
-
-	// Define iteration indices
-	int cellIdx, fieldIdx;
-
-	// Perform collision on all "inner" (FLUID) cells
-	for (int z = 1; z <= xlength ; z++) {
-		for (int y = 1; y <= xlength; y++) {
-			for (int x = 1; x <= xlength; x++) {
-
-				// Get the index of the first distribution
-				// in the current cell
-				fieldIdx = p_computeCellOffsetXYZ(x, y, z, xlength);
-				cellIdx = Q*fieldIdx;
-
-                
-                for (int i = 0; i < Q; ++i) {
-                    
-                //TODO: Do they stream to other cells in ref?
-                //          * We want to take distributions in.
-                collideField[cellIdx] = streamField[cellIdx];
-
-
-                }
-            }
-        }
-    }
-
-
-
-
-}
 
 int main(int argc, char *argv[]){
 
