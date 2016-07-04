5ee6647b7dba02d2b0673341aa5a5c03e6258a63
sonVishal <vishal.sontakke@tum.de>
2016-07-04 20:37:34 +0200

    Adapt streamCollide

--- srcProject/streamCollide.c
+++ srcProject/streamCollide.c
@@ -2,28 +2,25 @@
 #include "computeCellValues.h"
 
 
-void streamCollide(t_component *c, int xlength, int* flagField){
+void streamCollide(t_component *c, const t_procData * const procData) {
 
 	// Define iteration indices
-	int cellIdx, fieldIdx;
+	int cellIdx;
 
 	// Perform collision on all "inner" (FLUID) cells
-	for (int z = 1; z <= xlength ; z++) {
-		for (int y = 1; y <= xlength; y++) {
-			for (int x = 1; x <= xlength; x++) {
-				fieldIdx = p_computeCellOffsetXYZ(x, y, z, xlength);
-				cellIdx = Q*fieldIdx;
-
-				for(int k = 0; k < NUMCOMP; ++k){
+	for (int z = 1; z <= procData->xLength[2] ; z++) {
+		for (int y = 1; y <= procData->xLength[1]; y++) {
+			for (int x = 1; x <= procData->xLength[0]; x++) {
+				cellIdx = p_computeCellOffsetXYZ_Q(x, y, z, procData->xLength);
+				for(int k = 0; k < numComp; ++k){
 	                for (int i = 0; i < Q; ++i) {
 						// f_new[f_index_end] = f[f_index_beg]
 						//                    - (f[f_index_beg] - f_eq[f_index_beg])
 						//                    / tau;
-						//TODO: Check if this is correct:
-						int nextCellIndex = x+LATTICEVELOCITIES[i][0] + (y+LATTICEVELOCITIES[i][1])*(xlength+2) +
-											(z+LATTICEVELOCITIES[i][2])*(xlength+2)*(xlength+2);
+						int nextCellIndex = p_computeCellOffsetXYZ_Q(x+LATTICEVELOCITIES[i][0],
+							y+LATTICEVELOCITIES[i][1], z+LATTICEVELOCITIES[i][2], procData->xLength);
 
-						c[k].collideField[cellIdx+Q-i-1] = c[k].streamField[nextCellIndex*Q+Q-i-1] - (c[k].streamField[nextCellIndex*Q+Q-i-1] - c[k].feq[nextCellIndex*Q+Q-i-1])/c[k].tau;
+						c[k].collideField[cellIdx+Q-i-1] = c[k].streamField[nextCellIndex+Q-i-1] - (c[k].streamField[nextCellIndex+Q-i-1] - c[k].feq[nextCellIndex+Q-i-1])/c[k].tau;
 						assert(c[k].collideField[cellIdx+Q-i-1] > 0.0);
 	                }
 				}
@@ -32,4 +29,3 @@ void streamCollide(t_component *c, int xlength, int* flagField){
         }
     }
 }
-
