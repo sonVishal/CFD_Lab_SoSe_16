461b0ee18d543c37a712f51e09044dcbb86bfe21
tordks <tordks@hotmail.com>
2016-07-04 22:32:44 +0200

    Change to sukop settings

--- srcProject/LBDefinitions.h
+++ srcProject/LBDefinitions.h
@@ -6,8 +6,9 @@
 static const int Q = 19;
 
 // reference density
-static const double rhoRef = 0.693;
-static const int rho0   = 1;
+//static const double rhoRef = 0.693;  //Shan Chen
+static const double rhoRef = 200;  //Sukop
+static const int rho0   = 200;
 
 // Number of components is a global variable
 int numComp;
