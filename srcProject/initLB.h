39be6f7c7e7ce7cfaa315bdd4097ce46a8a87572
tordks <tordks@hotmail.com>
2016-07-06 17:09:13 +0200

    comment in initLB.h

--- srcProject/initLB.h
+++ srcProject/initLB.h
@@ -7,28 +7,28 @@
 #include <stdlib.h>
 #include <math.h>
 
-//// Read the number of components
+// Read the number of components
 void readNumComp(int argc, char *argv[]);
 
-//[> reads the parameters for the lid driven cavity scenario from a config file <]
 
 int readParameters(
-    int *xlength,                       // reads domain size. Parameter name: "xlength"
-    t_component *c,
+    int *xlength,                       // Reads domain size. Parameter name: "xlength"
+    t_component *c,                     // Contains information on the components of the simulation
     double G[numComp][numComp],         // Matrix giving interaction. name: "G00, G01, G10, G11,..."
     int *procsPerAxis,                  // Number of sub divisions in x,y,z directions
-    int *timesteps,                     // number of timesteps. Parameter name: "timesteps"
-    int *timestepsPerPlotting,          // timesteps between subsequent VTK plots. Parameter name: "vtkoutput"
-    int argc,                           // number of arguments. Should equal 2 (program + name of config file
+    int *timesteps,                     // Number of timesteps. Parameter name: "timesteps"
+    int *timestepsPerPlotting,          // Timesteps between subsequent VTK plots. Parameter name: "vtkoutput"
+    int argc,                           // Number of arguments. Should equal 2 (program + name of config file
     char *argv[]                        // argv[1] shall contain the path to the config file
 );
 
+// Initialize the fields for all the components
 void initialiseProblem(t_component *c, int *flagField, const t_procData * const procData);
 
-//[> Initialize the fields for all the components <]
+// Initialises fields for two components
 void initialiseComponents(t_component *c, const t_procData * const procData);
 
-/* initialises the particle distribution functions and the flagfield */
+// Initialises fields for one component
 void initialiseFields(t_component * c, const t_procData * const procData);
 
 #endif
