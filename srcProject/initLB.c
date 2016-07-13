6404dbb1c0ad4ab05a542fb373d7aebba88e8c73
sonVishal <vishal.sontakke@tum.de>
2016-07-13 06:45:30 +0200

    Add initial test case as per Shan-Chen

--- srcProject/initLB.c
+++ srcProject/initLB.c
@@ -159,13 +159,22 @@ void initialiseComponents(t_component *c, const t_procData * const procData) {
                 cellIdx = Q*fieldIdx;
 
 				rnd = (double)rand()/(double)RAND_MAX;
-				if (rnd < 0.5) {
-					c[0].rho[fieldIdx] = 0.0;
-					c[1].rho[fieldIdx] = c[1].rhoRef;
-				} else {
-					c[0].rho[fieldIdx] = c[0].rhoRef;
-					c[1].rho[fieldIdx] = 0.0;
-				}
+				c[0].rho[fieldIdx] = c[0].rhoRef - 0.5*0.01*c[0].rhoRef + 0.01*c[0].rhoRef*rnd;  // Shan Chen;
+				c[1].rho[fieldIdx] = c[1].rhoRef - 0.5*0.015*c[0].rhoRef + 0.015*c[0].rhoRef*rnd;
+				// if (rnd <= 0.5) {
+				// 	c[0].rho[fieldIdx] = c[0].rhoRef - c[0].rhoRef/100;
+				// 	c[1].rho[fieldIdx] = c[1].rhoRef - c[1].rhoRef/100;
+				// } else {
+				// 	c[0].rho[fieldIdx] = c[0].rhoRef + c[0].rhoRef/100;
+				// 	c[1].rho[fieldIdx] = c[1].rhoRef + c[1].rhoRef/100;
+				// }
+				// if (rnd <= 0.5) {
+				// 	c[0].rho[fieldIdx] = 0.0;
+				// 	c[1].rho[fieldIdx] = c[1].rhoRef;
+				// } else {
+				// 	c[0].rho[fieldIdx] = c[0].rhoRef;
+				// 	c[1].rho[fieldIdx] = 0.0;
+				// }
 				computeFeqCell(&c[0].rho[fieldIdx], u0, &c[0].feq[cellIdx]);
 				computeFeqCell(&c[1].rho[fieldIdx], u0, &c[1].feq[cellIdx]);
 
